1ede411a6cc4eb52109fa75acbee80a0583001bc
lbwww <2511302037@qq.com>
2020-11-19 09:43:51 +0800

    11.19（1）

--- lbww/src/com/neusoft/JDBCDemo2.java
+++ lbww/src/com/neusoft/JDBCDemo2.java
@@ -9,7 +9,7 @@ import java.sql.Statement;
  * JDBC连接mysql示例
  */
 
-public class JDBCDemo1 {
+public class JDBCDemo2 {
     public static void main(String[] args) throws Exception{
         // 1、导入驱动jar包, 建立libs文件夹， 右键 add as library
         // 2、 注册驱动
@@ -17,7 +17,7 @@ public class JDBCDemo1 {
         // 3、获取数据库连接对象
         Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jiangkeda","root","root");
         // 4、定义sql
-        String sql = "update account set balance = 500 where id = 1";
+        String sql = "insert into account values(null,'lei',666)";
         // 5、获取数据库连接对象statement
         Statement stmt = conn.createStatement();
         // 6、执行sql
--- lbww/src/com/neusoft/JDBCDemo4.java
+++ lbww/src/com/neusoft/JDBCDemo4.java
@@ -3,13 +3,14 @@ package com.neusoft;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 /**
  * JDBC连接mysql示例
  */
 
-public class JDBCDemo1 {
+public class JDBCDemo4 {
     public static void main(String[] args) throws Exception{
         // 1、导入驱动jar包, 建立libs文件夹， 右键 add as library
         // 2、 注册驱动
@@ -17,16 +18,18 @@ public class JDBCDemo1 {
         // 3、获取数据库连接对象
         Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jiangkeda","root","root");
         // 4、定义sql
-        String sql = "update account set balance = 500 where id = 1";
+        String sql = "select * from account";
         // 5、获取数据库连接对象statement
         Statement stmt = conn.createStatement();
         // 6、执行sql
-        int i = stmt.executeUpdate(sql);
+        ResultSet rs = stmt.executeQuery(sql);
         // 7、处理结果
-        if(i==1){
-            System.out.println("成功");
-        }else {
-            System.out.println("失败");
+        while (rs.next()) {
+            int columnlndex;
+            int id = rs.getInt("ID");
+            String name = rs.getString("NAME");
+            double balance = rs.getDouble("balance");
+            System.out.println(id + "----" + name + "----" + balance);
         }
         // 8、释放资源
         stmt.close();
